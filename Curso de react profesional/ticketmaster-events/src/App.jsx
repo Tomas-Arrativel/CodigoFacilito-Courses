9b6453cc1352dd19c662f172342cfd9f4752bcb3
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-28 07:52:30 -0300

    Cleaning app component

--- Curso de react profesional/ticketmaster-events/src/App.jsx
+++ Curso de react profesional/ticketmaster-events/src/App.jsx
@@ -1,28 +1,7 @@
-import { useRef, useState } from 'react';
-
-import Navbar from './components/Navbar';
-import Events from './components/Events';
 import Routes from './routes';
 import './App.css';
 
 function App() {
-  /*
-  const [searchText, setSearchText] = useState('');
-  const containerRef = useRef();
-
-  const handleNavbarSearch = (term) => {
-    console.log(containerRef.current);
-    setSearchText(term);
-  };
-
-  return (
-    <>
-      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-      <Events searchText={searchText} />
-      <SignUpForm  />
-    </>
-  );
-*/
   return <Routes />;
 }
 
--- Curso de react profesional/ticketmaster-events/src/views/Home/index.jsx
+++ Curso de react profesional/ticketmaster-events/src/views/Home/index.jsx
@@ -1,5 +1,23 @@
+import { useState, useRef } from 'react';
+
+import Navbar from '../../components/Navbar';
+import Events from '../../components/Events';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [searchText, setSearchText] = useState('');
+  const containerRef = useRef();
+
+  const handleNavbarSearch = (term) => {
+    console.log(containerRef.current);
+    setSearchText(term);
+  };
+
+  return (
+    <>
+      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
+      <Events searchText={searchText} />
+    </>
+  );
 };
 
 export default Home;
