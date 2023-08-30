e0dbe123f52be3507c1570ca8b68608b1cdc4388
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-30 08:57:52 -0300

    adding images to like events

--- Curso de react profesional/ticketmaster-events/src/components/Events/components/EventItem/index.jsx
+++ Curso de react profesional/ticketmaster-events/src/components/Events/components/EventItem/index.jsx
@@ -1,17 +1,33 @@
 import styles from './EventItem.module.css';
 
+import useLikeEvents from '../../../../hooks/useLikeEvents';
+import HearthFilled from '../../../../assets/hearth-filled.png';
+import HearthUnfilled from '../../../../assets/hearth-unfilled.png';
+
 const EventItem = ({ info, name, image, onEventClick, id }) => {
+  const { isEventLiked } = useLikeEvents(id);
+
   const handleSeeMoreClick = (e) => {
     e.stopPropagation();
     onEventClick(id);
   };
+
+  const handleHearthClick = () => {};
 
   return (
     <div
       onClick={() => console.log('Padre Clickeado')}
       className={styles.eventItemContainer}
     >
-      <img src={image} alt={name} width={400} height={250} />
+      <div className={styles.imagesContainer}>
+        <img
+          src={isEventLiked ? HearthFilled : HearthUnfilled}
+          alt='Like button'
+          className={styles.hearthImage}
+          onClick={handleHearthClick}
+        />
+        <img src={image} alt={name} width={400} height={250} />
+      </div>
       <div className={styles.eventInfoContainer}>
         <h4>{name}</h4>
         <p>{info}</p>
