9ed57df008dec35b8d94550f88b8b7489c133140
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-30 14:40:44 -0300

    MyInfo Page finished

--- Curso de react profesional/ticketmaster-events/src/views/Profile/components/MyInfo/index.jsx
+++ Curso de react profesional/ticketmaster-events/src/views/Profile/components/MyInfo/index.jsx
@@ -1,5 +1,71 @@
+import { useForm } from 'react-hook-form';
+import styles from './MyInfo.module.css';
+import { useEffect } from 'react';
+
+const USER_DATA = 'userData';
+
 const MyInfo = () => {
-  return <div>MyInfo</div>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  useEffect(() => {
+    try {
+      const userData = JSON.parse(localStorage.getItem(USER_DATA)) || {};
+      setValue('name', userData?.name);
+      setValue('email', userData?.email);
+      setValue('age', userData?.age);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const handleFormSubmit = (data) => {
+    try {
+      localStorage.setItem(USER_DATA, JSON.stringify(data));
+      alert('Usuario cargado correctamente');
+    } catch (error) {
+      alert('Ha ocurrido un error');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)} className={styles.form}>
+      <label className={styles.label}>
+        Name
+        <input
+          {...register('name', { required: true, min: 1, max: 120 })}
+          className={styles.input}
+        />
+      </label>
+      <label className={styles.label}>
+        Email
+        <input
+          {...register('email', { required: true, min: 1, max: 200 })}
+          className={styles.input}
+        />
+      </label>
+      <label className={styles.label}>
+        Age
+        <input
+          type='number'
+          {...register('age', {
+            required: true,
+            min: 1,
+            max: 130,
+            valueAsNumber: true,
+          })}
+          className={styles.input}
+        />
+      </label>
+      <button type='submit' className={styles.submitBtn}>
+        Save
+      </button>
+    </form>
+  );
 };
 
 export default MyInfo;
--- Curso de react profesional/ticketmaster-events/src/views/Profile/index.jsx
+++ Curso de react profesional/ticketmaster-events/src/views/Profile/index.jsx
@@ -1,9 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+import styles from './Profile.module.css';
 
 const Profile = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleTabClick = (path) => {
+    navigate(`/profile/${path}`);
+  };
+
   return (
     <div>
-      Profile
+      <Link to='/' className={styles.homeLink}>
+        Inicio
+      </Link>
+      <div className={styles.profileNavContainer}>
+        <span
+          className={`${pathname.includes('my-info') ? styles.active : ''} ${
+            styles.tab
+          }`}
+          onClick={() => handleTabClick('my-info')}
+        >
+          Mi información
+        </span>
+        <span
+          className={`${
+            pathname.includes('liked-events') ? styles.active : ''
+          } ${styles.tab}`}
+          onClick={() => handleTabClick('liked-events')}
+        >
+          Eventos favoritos
+        </span>
+      </div>
       <Outlet />
     </div>
   );
