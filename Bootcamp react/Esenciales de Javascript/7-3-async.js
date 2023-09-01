46ebaf5ba4f7bde18423026fd4bf13113e90957e
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-01 09:30:14 -0300

    7-3 async

--- Bootcamp react/Esenciales de Javascript/7-3-async.js
+++ Bootcamp react/Esenciales de Javascript/7-3-async.js
@@ -6,56 +6,54 @@ const wait = (name) =>
     setTimeout(() => {
       console.log(name);
       return resolve();
-    }, 100)
+    }, 100),
   );
 
-const validarPeticion = () => wait("validarPeticion");
-const formatearEmail = () => wait("formatearEmail");
-const formatearResultado = () => wait("formatearResultado");
-const actualizarDatosDeUsuario = () => wait("actualizarDatosDeUsuario");
+const validarPeticion = () => wait('validarPeticion');
+const formatearEmail = () => wait('formatearEmail');
+const formatearResultado = () => wait('formatearResultado');
+const actualizarDatosDeUsuario = () => wait('actualizarDatosDeUsuario');
 const enviarCorreo = async () => {
-  console.log("enviarCorreo");
+  console.log('enviarCorreo');
   throw new Error();
 };
 
 /**
  * Composición de un proceso complejo con chain.
  */
-// const proceso = () =>
-//   validarPeticion()
-//     .then(formatearEmail)
-//     .then(actualizarDatosDeUsuario)
-//     .then(() =>
-//       enviarCorreo()
-//         .catch(() => console.warn("Fallo correo")))
-//     .then(formatearResultado)
-//     .then(() => console.log("Fin de proceso"))
-//     .catch(() => console.error("Fallo"))
-//     .finally(() => console.log("Limpiar proceso"));
+const proceso = () =>
+  validarPeticion()
+    .then(formatearEmail)
+    .then(actualizarDatosDeUsuario)
+    .then(() => enviarCorreo().catch(() => console.warn('Fallo correo')))
+    .then(formatearResultado)
+    .then(() => console.log('Fin de proceso'))
+    .catch(() => console.error('Fallo'))
+    .finally(() => console.log('Limpiar proceso'));
 
 /**
  * Composición de un proceso complejo de forma imperativa.
  */
-const proceso = async () => {
-  try {
-    let valor = await validarPeticion();
-    await formatearEmail();
-    await actualizarDatosDeUsuario();
+// const proceso = async () => {
+//   try {
+//     let valor = await validarPeticion();
+//     await formatearEmail();
+//     await actualizarDatosDeUsuario();
 
-    try {
-      await enviarCorreo();
-    } catch (err) {
-      console.warn("Fallo correo");
-    }
+//     try {
+//       await enviarCorreo();
+//     } catch (err) {
+//       console.warn("Fallo correo");
+//     }
 
-    await formatearResultado();
-    console.log("Fin de proceso");
-  } catch (err) {
-    console.error("Fallo");
-  } finally {
-    console.log("Limpiar proceso");
-  }
-};
+//     await formatearResultado();
+//     console.log("Fin de proceso");
+//   } catch (err) {
+//     console.error("Fallo");
+//   } finally {
+//     console.log("Limpiar proceso");
+//   }
+// };
 
 /**
  * Playground.
