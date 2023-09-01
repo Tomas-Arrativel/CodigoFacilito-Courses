6e81e08596ad8a3762e8223a6147862a499fb58d
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-01 09:28:57 -0300

    asyncronous

--- Bootcamp react/Esenciales de Javascript/7-1-async.js
+++ Bootcamp react/Esenciales de Javascript/7-1-async.js
@@ -2,16 +2,16 @@
  * Top level await solo están disponibles en archivos mjs.
  */
 async function app() {
-  let promise = fetch("https://api.github.com/users/codigofacilito");
+  let promise = fetch('https://api.github.com/users/codigofacilito');
   console.log(promise);
 
   let result = await promise
     .then((result) => {
-      // throw new Error();
+      throw new Error();
       return result;
     })
     .catch((err) => console.error(err))
-    .finally(() => console.info("cleanup"));
+    .finally(() => console.info('cleanup'));
   console.log(result);
 }
 
