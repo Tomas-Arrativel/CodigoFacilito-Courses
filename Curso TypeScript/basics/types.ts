a98fdea0babd840012181b53f05f638541cb1659
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-04 08:18:57 -0300

    TypeScript types

---
+++ Curso TypeScript/basics/types.ts
@@ -0,0 +1,38 @@
+let myName: string = 'Tomás';
+let months: number = 12;
+let isSenior: boolean = false;
+let person: Object = {};
+
+// let fruits: Array<string> = ["123"];
+let fruits: string[] = ['123'];
+
+let response: any = 'Holaa';
+response = 20;
+response = true;
+response = ['Hola', {}, 123];
+
+function saludar(): void {
+  console.log('hola');
+}
+
+let response2: unknown;
+response2 = true;
+if (response2 === 'ealdlaedh') {
+}
+
+let response3 = null;
+let response4 = undefined;
+
+type ResponseTypes = number | undefined;
+let response5: ResponseTypes = 5;
+response5?.toString();
+
+let responseProducts: ResponseTypes;
+let responsePartners: ResponseTypes;
+
+responsePartners?.toString().concat('');
+
+// Type ASSERTION
+
+let message: any = '';
+let messageUpperCase = (message as string).toUpperCase();
