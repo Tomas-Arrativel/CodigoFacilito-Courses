2fd2889d48566971d4d806975627edcb4d7879e7
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-12 08:09:37 -0300

    excercise

--- Bootcamp react/Programacion funcional/3-5-hoc.js
+++ Bootcamp react/Programacion funcional/3-5-hoc.js
@@ -1,7 +1,5 @@
-const numeros = [1, 2, 3, 4, 5];
+const nums = [1, 2, 3, 4, 5];
 
-let suma = numeros
-  .filter((numero) => numero % 2 === 0)
-  .reduce((acc, numero) => acc + numero);
+let suma = nums.filter((num) => num % 2 === 0).reduce((acc, num) => acc + num);
 
 console.log(suma);
