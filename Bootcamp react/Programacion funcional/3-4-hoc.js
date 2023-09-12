ce21c72f6e23a07d68e2a722b016f91b9bc3369a
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-12 08:07:45 -0300

    HOF excercise

--- Bootcamp react/Programacion funcional/3-4-hoc.js
+++ Bootcamp react/Programacion funcional/3-4-hoc.js
@@ -2,7 +2,7 @@ const numeros = [1, 2, 3, 4, 5, 3, 2, 5, 6, 78];
 const numeros2 = [
   [1, 2],
   [2, 3, [2, 3]],
-  [2, 3]
+  [2, 3],
 ];
 
 function convertirNumerosEnStrings(nums) {
@@ -20,22 +20,8 @@ function sumarNumeros(nums) {
 /**
  * Playground
  */
-const agrupados = numeros.reduce(
-  (obj, n) => {
-    if (n > 5) {
-      var target = obj.mayores;
-      target = [...target, n];
-      return { ...obj, mayores: target };
-    } else {
-      target = obj.menores;
-      target = [...target, n];
-      return { ...obj, menores: target };
-    }
-  },
-  {
-    mayores: [],
-    menores: []
-  }
-);
+
+console.log(numeros2.flat());
+console.log(numeros2.flatMap((x) => x.toString));
 
 console.log(agrupados);
