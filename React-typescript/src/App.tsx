a487260b417931b87567ad9833a8fdfcb81361a9
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-15 09:58:03 -0300

    lists

--- React-typescript/src/App.tsx
+++ React-typescript/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import List from './components/List';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Sub {
+  nick: string;
+  avatar: string;
+  subMonths: number;
+  description?: string;
 }
 
-export default App
+const INITIAL_STATE = [
+  {
+    nick: 'Dapelu',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    description: 'Dapelu hace de moderador',
+  },
+  {
+    nick: 'sergio_serrano',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
+  },
+];
+
+function App() {
+  const [subs, setSubs] = useState<Array<Sub>>([]);
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Midu Subs</h1>
+      <List subs={subs} />
+    </div>
+  );
+}
+
+export default App;
