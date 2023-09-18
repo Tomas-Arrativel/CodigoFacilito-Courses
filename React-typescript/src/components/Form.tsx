008522a6c9cdd5c84649eb12a33893afc4af0c05
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-18 08:26:53 -0300

    events with ts

--- React-typescript/src/components/Form.tsx
+++ React-typescript/src/components/Form.tsx
@@ -1,13 +1,63 @@
-const Form = () => {
-  const handleSubmit = () => {};
+import { useState } from 'react';
+import { Sub } from '../types';
+
+interface FormState {
+  inputValues: Sub;
+}
+
+interface FormProps {
+  onNewSub: () => {};
+}
+
+const Form = ({ onNewSub }: FormProps) => {
+  const [inputValues, setInputValues] = useState<FormState['inputValues']>({
+    nick: '',
+    subMonths: 0,
+    avatar: '',
+    description: '',
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {};
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type='text' name='nick' placeholder='Nick' />
-        <input type='text' name='subMonths' placeholder='SubMonths' />
-        <input type='text' name='avatar' placeholder='Avatar' />
-        <input type='text' name='description' placeholder='Description' />
+        <input
+          onChange={handleChange}
+          value={inputValues.nick}
+          type='text'
+          name='nick'
+          placeholder='Nick'
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.subMonths}
+          type='number'
+          name='subMonths'
+          placeholder='SubMonths'
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.avatar}
+          type='text'
+          name='avatar'
+          placeholder='Avatar'
+        />
+        <textarea
+          onChange={handleChange}
+          value={inputValues.description}
+          name='description'
+          placeholder='Description'
+        />
 
         <button>Save new Sub!</button>
       </form>
