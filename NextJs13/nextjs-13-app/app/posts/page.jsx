b4ec02b6109600a357baab31f238cf1266a02c3b
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-20 17:48:20 -0300

    Loading page with NextJS

--- NextJs13/nextjs-13-app/app/posts/page.jsx
+++ NextJs13/nextjs-13-app/app/posts/page.jsx
@@ -3,12 +3,15 @@ import PostCard from '../../components/PostCard';
 const loadPosts = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
+
+  // await new Promise((resolve) => setTimeout(resolve, 1500));
   return data;
 };
 
 //RSC
 const PostsPage = async () => {
   const posts = await loadPosts();
+
   return (
     <div>
       {posts.map((post) => (
