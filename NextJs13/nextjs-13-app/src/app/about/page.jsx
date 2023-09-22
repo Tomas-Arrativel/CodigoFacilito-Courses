ea888c89e9c70549bc1c5835ca91bb4872e15bc6
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-22 09:12:44 -0300

    useRouter and useParams

--- NextJs13/nextjs-13-app/src/app/about/page.jsx
+++ NextJs13/nextjs-13-app/src/app/about/page.jsx
@@ -1,9 +1,38 @@
-export const metadata = {
-  title: 'About me',
-};
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 const AboutPage = () => {
-  return <h1>Hello from about</h1>;
+  const router = useRouter();
+
+  return (
+    <section>
+      <h1>Hello from about</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eius
+        ratione alias officiis maxime ipsum fugiat voluptatem dolore tempore
+        fugit, doloremque iusto corrupti iste iure sunt ea, placeat cum quaerat.
+        Repellat deserunt repudiandae quam sapiente accusantium inventore ad
+        voluptas voluptatum nobis facere. Maxime veritatis quisquam sint eaque
+        tempora exercitationem voluptas, ratione est molestiae commodi
+        reiciendis perferendis dolor pariatur amet possimus laborum, accusantium
+        tempore vero explicabo nisi error obcaecati dolorum? Est totam
+        asperiores nulla ex harum dolore odio velit debitis! Temporibus ipsum
+        quae corporis amet fugit autem mollitia incidunt obcaecati iste, dolore
+        consectetur exercitationem optio. Sequi culpa officiis veritatis
+        doloribus eveniet!
+      </p>
+
+      <button
+        className='about-btn'
+        onClick={() => {
+          router.push('/');
+        }}
+      >
+        Click
+      </button>
+    </section>
+  );
 };
 
 export default AboutPage;
