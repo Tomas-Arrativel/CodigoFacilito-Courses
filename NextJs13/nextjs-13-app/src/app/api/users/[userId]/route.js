e0458b5f101202172684e2a85b382613fc2bb5e8
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-22 09:53:32 -0300

    Search Params

--- NextJs13/nextjs-13-app/src/app/api/users/[userId]/route.js
+++ NextJs13/nextjs-13-app/src/app/api/users/[userId]/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(req, { params }) {
+  const { searchParams } = new URL(req.url);
+  console.log(searchParams);
+  console.log(searchParams.get('nombre'));
+  console.log(searchParams.get('apellido'));
+
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/users/${params.userId}`,
   );
