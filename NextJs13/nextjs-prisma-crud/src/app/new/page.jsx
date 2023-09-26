754389b4284fe451d8e06815b0c098af737e02aa
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-26 08:30:17 -0300

    Update tasks

--- NextJs13/nextjs-prisma-crud/src/app/new/page.jsx
+++ NextJs13/nextjs-prisma-crud/src/app/new/page.jsx
@@ -1,26 +1,51 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-function NewPage() {
+function NewPage({ params }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, []);
+
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
 
-    const res = await fetch('/api/tasks', {
-      method: 'POST',
-      body: JSON.stringify({ title, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = res.json();
-    console.log(data);
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+    } else {
+      const res = await fetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = res.json();
+    }
 
+    router.refresh();
     router.push('/');
   };
+
   return (
     <div className='h-screen flex justify-center items-center'>
       <form
@@ -35,6 +60,8 @@ function NewPage() {
           type='text'
           className='border border-gray-400 p-2 mb-4 w-full text-gray-900'
           placeholder='Titulo'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
 
         <label htmlFor='description' className='font-bold text-sm'>
@@ -45,7 +72,10 @@ function NewPage() {
           rows='4'
           className='border border-gray-400 p-2 mb-4 w-full text-gray-900'
           placeholder='Describe tu tarea'
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
+
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
           Crear
         </button>
