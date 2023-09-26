7f246367c9107a6290f14ee1eba63b5b5940a5e5
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-26 09:16:44 -0300

    aboutPage

--- NextJs13/nextjs-prisma-crud/src/app/about/page.jsx
+++ NextJs13/nextjs-prisma-crud/src/app/about/page.jsx
@@ -1,5 +1,22 @@
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <section className='container mx-auto'>
+      <h3 className='text-3xl my-5 font-bold'>About</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut similique
+        dolor culpa non praesentium ex sunt incidunt ratione, assumenda aliquid
+        quo iusto consectetur ipsum provident! Error blanditiis quae, neque
+        vitae ullam quas, debitis nemo, adipisci in perspiciatis unde
+        exercitationem ex magnam sed quidem porro enim velit facilis eos?
+        Eveniet eos nemo est repudiandae ut reiciendis optio repellendus
+        quibusdam quisquam incidunt cupiditate asperiores, consequuntur corrupti
+        consectetur laudantium. Quasi, iste totam blanditiis deserunt, et
+        molestiae dolores eos magni explicabo, modi veniam debitis atque impedit
+        sapiente. Deleniti, illo voluptatum. Quae doloribus, tempora et suscipit
+        deleniti tempore minus modi odit, quibusdam esse, ea ut?
+      </p>
+    </section>
+  );
 }
 
 export default AboutPage;
