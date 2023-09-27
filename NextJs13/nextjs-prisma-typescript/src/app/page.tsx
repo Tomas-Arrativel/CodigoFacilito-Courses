b78a4b0cca46b0eaa1bc67579fb62f91d2c187b4
Tomás Arrativel <arrativeltomas@gmail.com>
2023-09-27 10:41:55 -0300

    Showing tasks in the homepage

--- NextJs13/nextjs-prisma-typescript/src/app/page.tsx
+++ NextJs13/nextjs-prisma-typescript/src/app/page.tsx
@@ -1,5 +1,26 @@
-function HomePage() {
-  return <div>HomePage</div>;
+import { prisma } from '@/libs/prisma';
+
+async function loadTasks() {
+  const tasks = await prisma.task.findMany();
+  return tasks;
+}
+
+async function HomePage() {
+  const tasks = await loadTasks();
+
+  return (
+    <div className='grid grid-cols-3 gap-3 mt-5'>
+      {tasks.map((task) => (
+        <div
+          key={task.id}
+          className='bg-gray-900 p-3 hover:cursor-pointer hover:bg-gray-800'
+        >
+          <h3 className='font-bold text-xl'>{task.title}</h3>
+          <p className='text-slate-400'>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
