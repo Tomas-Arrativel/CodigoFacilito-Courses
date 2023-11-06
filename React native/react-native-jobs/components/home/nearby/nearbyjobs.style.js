d549d2774351ef0882f8c8fe1978e521a9aaab17
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-06 10:31:53 -0300

    styles changes made

--- React native/react-native-jobs/components/home/nearby/nearbyjobs.style.js
+++ React native/react-native-jobs/components/home/nearby/nearbyjobs.style.js
@@ -1,25 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-import { COLORS, FONT, SIZES } from "../../../constants";
+import { COLORS, SIZES } from '../../../constants';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: SIZES.xLarge,
   },
   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: SIZES.small,
   },
   headerTitle: {
     fontSize: SIZES.large,
-    fontFamily: FONT.medium,
+    fontWeight: '500',
     color: COLORS.primary,
   },
   headerBtn: {
     fontSize: SIZES.medium,
-    fontFamily: FONT.medium,
+    fontWeight: '500',
     color: COLORS.gray,
   },
   cardsContainer: {
