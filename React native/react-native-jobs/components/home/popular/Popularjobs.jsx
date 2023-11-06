40e6c3da5e52623e02b723c01067cb80f61811e7
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-06 10:33:01 -0300

    Nearby jobs

--- React native/react-native-jobs/components/home/popular/Popularjobs.jsx
+++ React native/react-native-jobs/components/home/popular/Popularjobs.jsx
@@ -21,6 +21,9 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
+  const [selectedJob, setSelectedJob] = useState();
+  const handleCardPress = () => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,7 +41,13 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                handleCardPress={handleCardPress}
+                selectedJob={selectedJob}
+                item={item}
+              />
+            )}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
